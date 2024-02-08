49c741b01064d7f4fbbf8e5793b2f320324312c4
soumik12345 <19soumik.rakshit96@gmail.com>
2024-02-08 15:47:30 +0000

    update: HF integration

---
+++ pegasusai/transformers/__init__.py
@@ -0,0 +1,25 @@
+from .loading import (
+    load_backbone,
+    load_config,
+    load_feature_extractor,
+    load_generation_config,
+    load_image_processor,
+    load_model,
+    load_pipeline,
+    load_processor,
+    load_tokenizer,
+    Task,
+)
+
+__all__ = [
+    "load_backbone",
+    "load_config",
+    "load_feature_extractor",
+    "load_generation_config",
+    "load_image_processor",
+    "load_model",
+    "load_pipeline",
+    "load_processor",
+    "load_tokenizer",
+    "Task",
+]
